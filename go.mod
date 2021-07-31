71993633a21ae2328a66bb1bd773752884b26529
Melbex De Leon <melbex11@gmail.com>
2021-07-31 11:59:45 +0800

    Improve loading speed and implement a bitset

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module alpacahq-take-home/m
 
 go 1.16
-
-require github.com/google/uuid v1.3.0
--- main.go
+++ main.go
@@ -7,11 +7,47 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"sync"
 )
 
+// valid value range for lottery picks
 var minimumValidPick uint = 1
 var maximumValidPick uint = 90
+
+var lottoPickLength = 5
+
+type BitSet map[uint]*[]bool
+
+// totalRecords could be a BitSet value
+func (bs *BitSet) CalculateWinners(nums *[]uint, minMatch uint, totalRecords uint) map[uint]uint {
+	winningPicksIndex := make(map[uint]*[]bool)
+	// winning picks
+	for _, v := range *nums {
+		winningPicksIndex[v] = (*bs)[v]
+	}
+
+	groupedWinnersCount := make(map[uint]uint)
+	for i := uint(len(*nums)); i >= 2; i-- {
+		groupedWinnersCount[i] = 0
+	}
+
+	for i := uint(0); i < totalRecords; i++ {
+		var noOfHits uint = 0
+		for _, index := range winningPicksIndex {
+			if (*index)[i] {
+				noOfHits += 1
+			}
+		}
+
+		if noOfHits >= minMatch {
+			groupedWinnersCount[noOfHits] += 1
+		}
+	}
+
+	return groupedWinnersCount
+}
 
 func main() {
 	// call file
@@ -22,33 +58,60 @@ func main() {
 		log.Fatal(err)
 	}
 
-	playerPicksIndex := lib.New(minimumValidPick, maximumValidPick, 10000000)
+	//playerPicksIndex := lib.New(minimumValidPick, maximumValidPick, 10000000)
 
-	var recordCount uint
+	bitSet := make(BitSet)
+	for i := minimumValidPick; i <= maximumValidPick; i++ {
+		boolSet := make([]bool, 10000000)
+		bitSet[i] = &boolSet
+	}
+
+	var recordCount uint = 0
+	var bufferOffset int64 = 0
+
 	for {
-		var lottoPick1 uint
-		var lottoPick2 uint
-		var lottoPick3 uint
-		var lottoPick4 uint
-		var lottoPick5 uint
-		_, err2 := fmt.Fscanf(file, "%d %d %d %d %d\n", &lottoPick1, &lottoPick2, &lottoPick3, &lottoPick4, &lottoPick5)
-		if errors.Is(err2, io.EOF) {
+		fileReadBuffer := make([]byte, 4096*3)
+		_, fileReadError := file.ReadAt(fileReadBuffer, bufferOffset)
+		if errors.Is(fileReadError, io.EOF) {
 			break
 		}
-		picks := []uint{lottoPick1, lottoPick2, lottoPick3, lottoPick4, lottoPick5}
 
-		isLottoPickValid := picksValid(picks)
-
-		// skip adding it to the record if it is not valid
-		if !isLottoPickValid {
-			continue
+		// find the last complete line scanned
+		for i := len(fileReadBuffer) - 1; i > 0; i -= 1 {
+			if string(fileReadBuffer[i]) == "\n" {
+				fileReadBuffer = fileReadBuffer[:i]
+				break
+			}
 		}
+		bufferOffset += int64(len(fileReadBuffer) + 1)
 
-		for i := 0; i < len(picks); i++ {
-			pick := picks[i]
-			playerPicksIndex.BitWiseOr(pick, recordCount)
+		stringifiedLottoRecords := string(fileReadBuffer)
+		records := strings.Split(stringifiedLottoRecords, "\n")
+		for _, record := range records {
+			lottoPicks := make([]string, 0)
+			for _, s := range strings.Split(record, " ") {
+				if s != "" {
+					lottoPicks = append(lottoPicks, s)
+				}
+			}
+
+			formattedLottoPicks := make([]uint, 0)
+			for _, lottoPick := range lottoPicks {
+				formattedLottoPick, _ := strconv.Atoi(lottoPick)
+				validLottoPickFormat := uint(formattedLottoPick)
+				formattedLottoPicks = append(formattedLottoPicks, validLottoPickFormat)
+			}
+
+			if picksValid(formattedLottoPicks) {
+				for _, flt := range formattedLottoPicks {
+					boolMap := bitSet[flt]
+					(*boolMap)[recordCount] = true
+				}
+
+				recordCount += 1
+			}
+
 		}
-		recordCount += 1
 	}
 
 	if fileCloseError := file.Close(); fileCloseError != nil {
@@ -85,7 +148,8 @@ func main() {
 
 		fmt.Println(winningPicks)
 
-		answersMap := calculateWinners(winningPicks, playerPicksIndex, recordCount)
+		//answersMap := calculateWinners(winningPicks, playerPicksIndex, recordCount)
+		answersMap := bitSet.CalculateWinners(&winningPicks, 2, recordCount)
 
 		for i := 5; i >= 2; i-- {
 			fmt.Printf("%d: %d\n", i, answersMap[uint(i)])
@@ -134,7 +198,7 @@ func picksValid(picks []uint) bool {
 	picksMap := make(map[uint]int)
 
 	for _, v := range picks {
-		if !isNumberValid(v) {
+		if !isNumberValid(int(v)) {
 			return false
 		}
 
@@ -150,8 +214,8 @@ func picksValid(picks []uint) bool {
 	return true
 }
 
-func isNumberValid(val uint) bool {
-	if val >= minimumValidPick && val <= maximumValidPick {
+func isNumberValid(val int) bool {
+	if val >= int(minimumValidPick) && val <= int(maximumValidPick) {
 		return true
 	}
 
