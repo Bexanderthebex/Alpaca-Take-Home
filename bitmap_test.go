079e6620dbc0b3d1a3d37deb141e50e28466732c
Melbex De Leon <melbex11@gmail.com>
2021-08-02 17:53:39 +0800

    Add test for BitMap

--- bitmap_test.go
+++ bitmap_test.go
@@ -1,27 +1,25 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
-func BenchmarkBoolMap_SetValue(b *testing.B) {
-	boolMap := NewBoolMap(1, 90, b.N)
+func BenchmarkBitMap_SetValue(b *testing.B) {
+	boolMap := NewBitMap(1, 90, b.N)
 
 	for i := 0; i < b.N; i++ {
 		boolMap.SetValue(uint(90), uint(i), true)
 	}
 }
 
-func BenchmarkBoolMap_GetValue(b *testing.B) {
-	boolMap := NewBoolMap(1, 90, b.N)
+func BenchmarkBitMap_GetValue(b *testing.B) {
+	boolMap := NewBitMap(1, 90, b.N)
 
 	for i := 0; i < b.N; i++ {
 		boolMap.GetValue(uint(90), uint(i))
 	}
 }
 
-func TestCorrectGetValue_BoolMap(t *testing.T) {
-	boolMap := NewBoolMap(1, 90, 100)
+func TestCorrectGetValue(t *testing.T) {
+	boolMap := NewBitMap(1, 90, 100)
 
 	boolMap.SetValue(90, 3, true)
 	boolMap.SetValue(78, 3, true)
@@ -31,8 +29,8 @@ func TestCorrectGetValue_BoolMap(t *testing.T) {
 	assertEqualBool(t, false, boolMap.GetValue(1, 3), "Incorrect value returned")
 }
 
-func TestCorrectGetTotalRecords_BoolMap(t *testing.T) {
-	boolMap := NewBoolMap(1, 90, 100)
+func TestCorrectGetTotalRecords(t *testing.T) {
+	boolMap := NewBitMap(1, 90, 100)
 
 	for i := 0; i < 10; i++ {
 		boolMap.IncrementTotalRecords()
